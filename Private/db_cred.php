e175470e327a0adb55a288f0e898875b9761fdd3
OsmanPoonawala <45496552+OsmanPoonawala@users.noreply.github.com>
2020-11-02 17:17:50 +0500

    connecting database

---
+++ Private/db_cred.php
@@ -0,0 +1,8 @@
+<?php
+
+define("DB_SERVER", "nnmeqdrilkem9ked.cbetxkdyhwsb.us-east-1.rds.amazonaws.com");
+define("DB_USER", "cjfexus9740iz1el");
+define("DB_PASS", "s15dt048p466uor9");
+define("DB_NAME", "hvfls63hwciux36p");
+
+?>
---
+++ Public/index.php
@@ -0,0 +1,4 @@
+<?php require_once('../private/initialise.php'); ?>
+<?php include(SHARED_PATH . '/header.php'); ?>
+    </body>
+</html>
